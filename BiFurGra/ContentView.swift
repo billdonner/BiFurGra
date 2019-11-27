d0782a179f306d73696ac82a183e02ff7d0fe52e
bill donner <billdonner@gmail.com>
2019-11-27 12:22:11 -0500

    Initial Commit

---
+++ BiFurGra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BiFurGra
+//
+//  Created by bill donner on 11/27/19.
+//  Copyright © 2019 bill donner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
